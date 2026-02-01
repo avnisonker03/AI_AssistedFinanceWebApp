035cdb5acd5239199a4a3e1df1afc53ce5ab77dc
avnisonker <avni.sonker@sportsdunia.com>
2026-02-01 23:11:56 +0530

    fixes

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ const app=express();
 // }))
 app.use(cors({
   origin: true,
-  credentials: true,
+  credentials: false,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
 }));
